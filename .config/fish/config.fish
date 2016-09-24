67a98cba983a55da76ac7e301a8530c0a411d3d5
Salam Xiyali <hiyali920@gmail.com>
2016-09-24 18:17:33 +0600

    added kill functions and changed to new ssh/sshuttle options

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,23 +1,3 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/salamjan/.config/omf"
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
-# Path to Oh My Fish install.
-### set -gx OMF_PATH "/home/salamjan/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/salamjan/.config/omf"
-
-# Load oh-my-fish configuration.
-### source $OMF_PATH/init.fish
-
 # set -gx JAVA_HOME /work/zulu8.15.0.1-jdk8.0.92-linux_x64
 set -gx JAVA_HOME /usr/lib/jvm/java-1.8.0-openjdk-amd64/
 
@@ -39,14 +19,37 @@ set -gx PATH $JAVA_HOME/bin \
 
 ### alias nautilus='nautilus --no-desktop &'
 
-function  kar
+function  skar
   kill -9  (lsof -i :8888 |grep -i ssh |  awk '{print $2}' | uniq)
   ssh  -fCND 8888 salamjan@seoul
 end
 
-function  xkar
+function  akar
   kill -9  (lsof -i :8888 |grep -i ssh |  awk '{print $2}' | uniq)
   ssh -i ~/.ssh/amazon.pem -fCND 8888 ubuntu@xiyali-aws
+end
+
+function  gkar
+  kill -9  (lsof -i :8888 |grep -i ssh |  awk '{print $2}' | uniq)
+  ssh -i ~/.ssh/gcs -fCND 8888 hiyali920@xiyali-gcs
+end
+
+function  kar-kill
+  kill -9  (lsof -i :8888 |grep -i ssh |  awk '{print $2}' | uniq)
+end
+
+function  gshutv
+  kill -9 (ps -ef | grep -i '\-\-dns' | awk '{print $2}' | uniq)
+  sshuttle -r hiyali920@xiyali-gcs 0.0.0.0/0 -vv -e "ssh -A -i /home/salamjan/.ssh/gcs" --dns
+end
+
+function  gshut
+  kill -9 (ps -ef | grep -i '\-\-dns' | awk '{print $2}' | uniq)
+  sshuttle --dns -vDr hiyali920@xiyali-gcs 0.0.0.0/0 -e "ssh -A -i /home/salamjan/.ssh/gcs"
+end
+
+function shut-kill
+  kill -9 (ps -ef | grep -i '\-\-dns' | awk '{print $2}' | uniq)
 end
 
 alias clip='xclip -sel clip'
@@ -73,26 +76,12 @@ export LC_ALL=en_US.UTF-8
 # #######################
 # for oh-my-fish
 # #######################
-#
 
-#
-#
-# Path to your oh-my-fish.
-### set -g OMF_PATH $HOME/.local/share/omf
-#
-# # Path to your oh-my-fish configuration.
-### set -g OMF_CONFIG $HOME/.config/omf
-#
-# fish_vi_mode
-### set --export EDITOR "vim"
-#
-#
-# ### Configuration required to load oh-my-fish ###
-# # Note: Only add configurations that are required to be set before oh-my-fish is loaded.
-# # For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
-# # to create a custom plugin instead.
-#
-# # Load oh-my-fish configuration.
-### source $OMF_PATH/init.fish
-#
-# Theme bobthefish
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
