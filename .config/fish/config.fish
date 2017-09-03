5a74b6c8a1acef0fbc0181c8f304261b5ffe5968
Salam Hiyali <hiyali920@gmail.com>
2017-09-03 16:15:05 +0800

    update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,61 +1,6 @@
-# set -gx JAVA_HOME /work/zulu8.15.0.1-jdk8.0.92-linux_x64
-set -gx JAVA_HOME /usr/lib/jvm/java-1.8.0-openjdk-amd64/
 
-set -gx PATH $JAVA_HOME/bin \
-        /usr/ \
-        /usr/bin \
-        /ENV/gradle-3.1/bin \
-        $PATH \
-        ~/.config/composer/vendor/bin
-
-        # /ENV/android-sdk-linux \
-        # /ENV/android-sdk-linux/tools \
-        # /ENV/android-sdk-linux/platform-tools \
-        # /ENV/android-sdk-linux/build-tools/23.0.0/ \
-        # $HOME/vuze \
-## set -gx ANDROID_HOME /ENV/android-studio/ /work/android-studio/bin/
-# set -gx ANDROID_HOME /ENV/android-sdk-linux/
-
-
-### alias nautilus='nautilus --no-desktop &'
-
-function  skar
-  kill -9  (lsof -i :8888 |grep -i ssh |  awk '{print $2}' | uniq)
-  ssh  -fCND 8888 salamjan@seoul
-end
-
-function  akar
-  kill -9  (lsof -i :8888 |grep -i ssh |  awk '{print $2}' | uniq)
-  ssh -i ~/.ssh/aws.pem -fCND 8888 ubuntu@xiyali-aws
-end
-
-
-
-####################### initial ######################
-
-function  gkar
-  kill -9  (lsof -i :8888 |grep -i ssh |  awk '{print $2}' | uniq)
-  ssh -i ~/.ssh/gcs -fCND 8888 hiyali920@xiyali-gcs
-end
-
-function  kar-kill
-  kill -9  (lsof -i :8888 |grep -i ssh |  awk '{print $2}' | uniq)
-end
-
-function  gshutv
-  kill -9 (ps -ef | grep -i '\-\-dns' | awk '{print $2}' | uniq)
-  sshuttle -r hiyali920@xiyali-gcs 0.0.0.0/0 -vv -e "ssh -A -i /home/salamjan/.ssh/gcs" --dns
-end
-
-function  gshut
-  kill -9 (ps -ef | grep -i '\-\-dns' | awk '{print $2}' | uniq)
-  sshuttle --dns -vDr hiyali920@xiyali-gcs 0.0.0.0/0 -e "ssh -A -i /home/salamjan/.ssh/gcs"
-end
-
-function shut-kill
-  kill -9 (ps -ef | grep -i '\-\-dns' | awk '{print $2}' | uniq)
-end
-
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
 
 alias tmux="env TERM=screen-256color-bce tmux"
@@ -67,28 +12,39 @@ alias cnpm="npm --registry=https://registry.npm.taobao.org \
 alias vim="nvim"
 alias vi="nvim"
 
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
 
-########################### initial done ############################
+function  gkar
+  kill -9  (lsof -i :5555 |grep -i ssh |  awk '{print $2}' | uniq)
+  ssh -i ~/.ssh/gcs -fCND 5555 hiyali920@hiyali-gcs
+end
+function kar-kill
+  kill -9  (lsof -i :5555 |grep -i ssh |  awk '{print $2}' | uniq)
+end
+
+function  gshut
+  kill -9 (ps -ef | grep -i '\-\-dns' | awk '{print $2}' | uniq)
+  sshuttle --dns -vDr hiyali920@hiyali-gcs 0.0.0.0/0 -e "ssh -A -i ~/.ssh/gcs"
+end
+function  gshutv
+  kill -9 (ps -ef | grep -i '\-\-dns' | awk '{print $2}' | uniq)
+  sshuttle -r hiyali920@hiyali-gcs 0.0.0.0/0 -vv -e "ssh -A -i ~/.ssh/gcs" --dns
+end
+function shut-kill
+  kill -9 (ps -ef | grep -i '\-\-dns' | awk '{print $2}' | uniq)
+end
 
 
-alias clip='xclip -sel clip'
-export NVIM_TUI_ENABLE_TRUE_COLOR=1
 
-set -gx MAVEN_OPTS "-DsocksProxyHost=127.0.0.1 -DsocksProxyPort=8888"
-set -gx BOOT_JVM_OPTIONS "-DsocksProxyHost=127.0.0.1 -DsocksProxyPort=8888"
-
-
-# #######################
-# for oh-my-fish
-# #######################
 
 # Path to Oh My Fish install.
 set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-# Load oh-my-fish configuration.
+# Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
 
+# Go
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/.go
+export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
